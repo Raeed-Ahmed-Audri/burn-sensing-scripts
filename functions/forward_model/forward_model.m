bccecbbd9444d8635d01326a4526898b014db2c8
Raeed-Ahmed-Audri <audrothewise@gmail.com>
2026-09-22 02:18:48 -0600

    Forward Model to calculate Z0, complex propagation, and epsilon are made

--- functions/forward_model/forward_model.m
+++ functions/forward_model/forward_model.m
@@ -1,38 +1,47 @@
-function [interpolated_complex_value] = load_itis_dispersion(filepath, frequency)
+function [interpolated_complex_value, Z0_material, gamma_epsilon_material] = fwrd_model_values(filepath, f)
     filepath = readtable(filepath);
+    m = numel(f);
+
+    %Initialize loop values
+    Z0_material = zeros(1, m);
+    gamma_epsilon_material = zeros(1, m);
+    interpolated_complex_value = zeros(1, m);
+    
+    %Collecting Data from File
     freq = filepath{:,1};
     freq_HZ = transpose(freq*10^9);
     real_permittivity = filepath{:,2};
     imaginary_permittivity = filepath{:,3};
+
+    %Manipulate Data to transpose as interp1 only works with a
+    %certain type of matrix
     complex_numbers = real_permittivity -j*imaginary_permittivity;
     complex_numbers_transpose = transpose(complex_numbers);
-    
-    %Interpolate the data
-    interpolated_complex_value = interp1(freq_HZ,complex_numbers_transpose, frequency);
-  
-end
-
-function [Z0_material] =  characteristic_impedance_material(filepath, f)
-    eps_complex = load_itis_dispersion(filepath,f);
-    Z0_material = 377./sqrt(eps_complex) 
-
-end
-
-function [gamma_epsilon] = gamma_epsilon_material(filepath, f)
-    eps_complex = load_itis_dispersion(filepath,f);
     c = physconst("LightSpeed");
-    gamma_epsilon = j*2*pi*f.*sqrt(eps_complex)./c
-end
 
-function [interpolated_complex_value, Z0_material, gamma_epsilon_material] = fwrd_model_values(filenames, f)
-    n = numel(filenames)
+
+    for i = 1:m
+        %Now we do our operations
+        
+
+        %Interpolate Frequencies
+        interpolated_complex_value(i) = interp1(freq_HZ, complex_numbers_transpose, f(:,i));
+        
+        %Calculate Characteristic Impedance
+        Z0_material(i) = 377./sqrt(interpolated_complex_value(i));
+        
+        %Calculate the Complex Propagation Constant
+        gamma_epsilon_material(i) = j*2*pi*f(:,i).*sqrt(interpolated_complex_value(i))./c;
+        
+
+    end
 
 end
     
-https://www.mathworks.com/matlabcentral/answers/2100341-iterating-over-a-cell-array-do-for-loops-work
-f = [3,5,6]*10^9;
-f1 = 3*10^9;
-filenames = ["Skin_edit.txt","Skin_edit.txt" ];
-fwrd_model_values(filenames,f)
+% f = [3,5,6]*10^9;
+% f1 = 3*10^9;
+% filenames = "Skin_edit.txt";
+% fwrd_model_values(filenames,f);
 %characteristic_impedance_material(filename,f);
 %gamma_epsilon_material("Skin_edit.txt",f);
+
